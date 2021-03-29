1aa954fcce29d129b2afc1a38d51cfd82c65e06f
Stephen Buchanan <stephen.buchanan@random.studio>
2021-03-29 16:12:56 +0200

    typeof defaults

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import defaults from "./defaults";
 import parse from "parse-strings-in-object";
 import rc from "rc";
 
-const config = parse(rc("lights", defaults));
+const config: typeof defaults = parse(rc("lights", defaults));
 
 console.log("started with config", config);
