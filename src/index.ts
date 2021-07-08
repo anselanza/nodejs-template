b4c0b0ffad8793b007d0896ca6308aec8be698e1
Stephen Buchanan <stephen.buchanan@random.studio>
2021-07-08 13:11:22 +0200

    app name

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import parse from "parse-strings-in-object";
 import rc from "rc";
 import { getLogger } from "log4js";
 
-const config: typeof defaults = parse(rc("lights", defaults));
+const appName = "app";
 
-const logger = getLogger();
+const config: typeof defaults = parse(rc(appName, defaults));
+
+const logger = getLogger(appName);
 logger.level = config.loglevel;
 
 logger.info("started with config", config);
